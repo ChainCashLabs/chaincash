65a16451a896177866f9a4b3334e32d915a1fea4
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2023-04-05 14:00:18 +0300

    removing unused code

--- src/main/scala/chaincash/offchain/SigUtils.scala
+++ src/main/scala/chaincash/offchain/SigUtils.scala
@@ -1,69 +1,25 @@
 package chaincash.offchain
 
-import chaincash.contracts.Constants
 import kiosk.ErgoUtil.randBigInt
-import org.ergoplatform.P2PKAddress
-import scorex.crypto.hash.Blake2b256
-import sigmastate.eval.CGroupElement
 import sigmastate.interpreter.CryptoConstants
 import special.sigma.GroupElement
 import sigmastate.eval._
-import sigmastate.serialization.GroupElementSerializer
-
 import scala.annotation.tailrec
 
 object SigUtils {
 
   @tailrec
-  def sign(msg: Array[Byte], sk: BigInt): (GroupElement, BigInt) = {
+  def sign(msg: Array[Byte], secretKey: BigInt): (GroupElement, BigInt) = {
     val r = randBigInt
-    println(s"secret: ${r.toString(16)}")
     val g: GroupElement = CryptoConstants.dlogGroup.generator
     val a: GroupElement = g.exp(r.bigInteger)
-    val z = (r + sk * BigInt(scorex.crypto.hash.Blake2b256(msg))) % CryptoConstants.groupOrder
+    val z = (r + secretKey * BigInt(scorex.crypto.hash.Blake2b256(msg))) % CryptoConstants.groupOrder
 
     if(z.bitLength <= 255) {
       (a, z)
     } else {
-      sign(msg,sk)
+      sign(msg,secretKey)
     }
   }
 
-  /* todo: remove
-  @tailrec
-  def signOld(msg: Array[Byte], sk: BigInt): (BigInt, BigInt) = {
-    val r = randBigInt
-    println(s"secret: ${r.toString(16)}")
-    val U: GroupElement = g.exp(r.bigInteger)
-    val c = BigInt(Blake2b256.hash(GroupElementSerializer.toBytes(U) ++ msg))
-    val s = r - c * sk
-    if(c.bitLength <= 255 && s.bitLength <= 255) {
-      (c, s)
-    } else {
-      println("Re-generating signature")
-      signOld(msg, sk)
-    }
-  }
-
-  def verifyOld(msg: Array[Byte], signature: (BigInt, BigInt), pk: GroupElement): Boolean = {
-    val c = signature._1
-    val s = signature._2
-    val U = g.exp(s.bigInteger).multiply(pk.exp(c.bigInteger))
-    c == BigInt(Blake2b256.hash(GroupElementSerializer.toBytes(U) ++ msg))
-  } */
-
-}
-
-object SigTester extends App {
-  import SigUtils._
-  import Constants.g
-
-  val sk = randBigInt
-
-  val msg = "hello".getBytes("UTF-8")
-
-  val sig = sign(msg, sk)
-  val pk = g.exp(sk.bigInteger)
-
- // println(verify(msg, sig, pk))
 }
